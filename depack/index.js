c6433216d8fa45ae9ad42028b9d452521eeba11a
Anton <anton@adc.sh>
2019-05-11 16:54:44 +0300

    types

--- depack/index.js
+++ depack/index.js
@@ -11,19 +11,49 @@ class Koa extends _Koa {
    * Shorthand for:
    *
    *    http.createServer(app.callback()).listen(...)
-   *
    * @return {http.Server}
    */
   listen(...args) {
     return super.listen(...args)
   }
+  /**
+   * Return JSON representation.
+   * @returns {{ subdomainOffset?: number, proxy?: boolean, env: string }}
+   */
+  toJSON() {
+    return super.toJSON()
+  }
+  /**
+   * Inspect implementation.
+   */
+  inspect() {
+    return this.toJSON()
+  }
+  /**
+   * Use the given middleware `fn`.
+   *
+   * Old-style middleware will be converted.
+   *
+   * @param {Middleware} fn
+   */
+  use(fn) {
+    return super.use(fn)
+  }
+  /**
+   * Return a request handler callback
+   * for node's native http server.
+   * @returns {function(!http.IncomingMessage, !http.ServerResponse): !Promise}
+   */
+  callback() {
+    return super.callback()
+  }
 }
-
-console.log('testing depack')
 
 module.exports = Koa
 
 /**
- * @suppress {nonStandardJsDocs}
  * @typedef {import('http').Server} http.Server
+ * @typedef {import('http').IncomingMessage} http.IncomingMessage
+ * @typedef {import('http').ServerResponse} http.ServerResponse
+ * @typedef {import('../types').Middleware} Middleware
  */
