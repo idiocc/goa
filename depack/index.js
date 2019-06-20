1cdb6317bdfde517d8d321640a704f9c41fd2784
Anton <anton@adc.sh>
2019-06-21 00:00:49 +0300

    i typedefs

--- depack/index.js
+++ depack/index.js
@@ -1,16 +1,1 @@
 export { default } from '../src'
-
-/**
- * @typedef {import('http').Server} http.Server
- * @typedef {import('http').IncomingMessage} http.IncomingMessage
- * @typedef {import('http').ServerResponse} http.ServerResponse
- *
- * @typedef {import('../types/vendor/cookies').Keygrip} Keygrip
- * @typedef {import('../types/typedefs/application').Middleware} Middleware
- * @typedef {import('../types/typedefs/application').Application} Application
- * @typedef {import('../types/typedefs/context').KoaContext} Context
- * @typedef {import('../types/typedefs/request').Request} Request
- * @typedef {import('../types/typedefs/request').ContextDelegatedRequest} ContextDelegatedRequest
- * @typedef {import('../types/typedefs/response').Response} Response
- * @typedef {import('../types/typedefs/response').ContextDelegatedResponse} ContextDelegatedResponse
- */
